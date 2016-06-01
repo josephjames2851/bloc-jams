3894bcc2d60bb3a72bfd1845f4ab2d1f601c325d
Joe Weinzettel <josephjames2851@gmail.com>
2016-06-01 12:32:47 -0500

    Assignment 22: Partially Completed.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,16 @@
 var animatePoints = function () {
     var points = document.getElementsByClassName('point');
+    
+    var revealPoint = function (points) {
+        for (var i = 0; i < points.length; i++) {
+            points[i].style.opacity = 1;
+            points[i].style.style.transform = "scaleX(1) translateY(0)";
+            points[i].style.mstransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+    };
 
-    var revealFirstPoint = function () {
+    /* var revealFirstPoint = function () {
         points[0].style.opacity = 1;
         points[0].style.transform = "scaleX(1) translateY(0)";
         points[0].style.mstransform = "scaleX(1) translateY(0)";
@@ -20,9 +29,12 @@ var animatePoints = function () {
         points[2].style.transform = "scaleX(1) translateY(0)";
         points[2].style.mstransform = "scaleX(1) translateY(0)";
         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+    }; 
+    
     revealFirstPoint();
     revealSecondPoint();
-    revealThirdPoint();
+    revealThirdPoint();  */
+    
+    revealPoint();
 };
 
