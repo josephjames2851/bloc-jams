abc2e9ffa2eefe77eb8f70a96162cec2b32d9fe0
Joe Weinzettel <josephjames2851@gmail.com>
2016-06-06 23:12:07 -0700

    Checkpoint 23: Needs De-bugging

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,8 @@
-var animatePoints = function () {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+
+var animatePoints = function(points) {
+    // var points = document.getElementsByClassName('point');
     
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
@@ -8,35 +11,24 @@ var animatePoints = function () {
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
 
-    for (var i = 0; i < points.length; i++) {
+    for(var i = 0; i < points.length; i++) {
         revealPoint(i);
     };
-        
-    /* var revealFirstPoint = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.mstransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealSecondPoint = function () {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.mstransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealThirdPoint = function () {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.mstransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }; 
-    
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();  */
-    
-    
 };
+
+window.onload = function() {
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener("scroll", function(event) {
+        /* console.log("Current offset from the top is " sellingPoints.getBoundingClientRect().top + " pixels"); */
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
 
